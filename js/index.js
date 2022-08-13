8faa94e9a9eb4e7a2bdd0a4ba02cfbc3f6b97ba7
DannaTorres22 <104436270+DannaTorres22@users.noreply.github.com>
2022-08-13 00:13:28 -0500

    Taller0_Final

---
+++ js/index.js
@@ -0,0 +1,7 @@
+'use strick'
+const icono = document.querySelector('.hamb')
+const menu = document.querySelector('.menu')
+
+icono.addEventListener("click", ()=>{
+      menu.classList.toggle("active")
+})
